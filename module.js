41e621f0dde77526b320af5d59ce13132277bf1c
syedazainabt <zainab.mulan@gmail.com>
2025-09-23 11:57:09 +0700

    Update module.js

--- module.js
+++ module.js
@@ -1,11 +1,10 @@
 let ax = require("axios");
 let randnum = Math.floor(Math.random() * 114) + 1;
-function rands_a(){
+export default function rands_a(){
 ax.get(`http://api.alquran.cloud/v1/surah/${randnum}`).then(res => {
         randAyah = Math.floor(Math.random() * res.data.data.numberOfAyahs) + 1;
         return "Random surah: Surah ", res.data.data.englishName, "Random ayah: Ayah number ", randAyah;
 }).catch(err => {
-    console.log(`error oof ${err}`);
+    return `error oof ${err}`;
 });
 }
-module.exports = rands_a;
