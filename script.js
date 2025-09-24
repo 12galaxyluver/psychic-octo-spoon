2e32f08c9ed5f5e83f343461adf89896007e53a3
syedazainabt <zainab.mulan@gmail.com>
2025-09-24 11:32:28 +0700

    external js converted from internal js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+import { rands_a } from "./module.js";
+  document.QuerySelector("b").onclick = () => {
+    rands_a();
+  }
+rands_a().then(msg => {
+  document.getElementById("s_a").innerHTML = msg;
+}).catch(err => {
+   document.getElementById("s_a").innerHTML = "Error: "+err;
+});
