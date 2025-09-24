545d84172d8ffc16be62e91194d7ff3250886401
syedazainabt <zainab.mulan@gmail.com>
2025-09-24 12:01:47 +0700

    update function call, queryselector misspelling and getelementbyid

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 import { rands_a } from "./module.js";
-document.QuerySelector("b").onclick = () => {
-  rands_a();
-}
+document.QuerySelector("b").addEventListener("click", rands_a);
+
 async function gets_a(){
   try {
     r = await rands_a();
-    document.getQuerySelector("s_a").innerHTML = `Surah and ayah: {msg}`;
+    document.QuerySelector("s_a").innerHTML = `Surah and ayah: {msg}`;
   }
   catch (e){
-    document.getQuerySelector("s_a").innerHTML = `Error: {e}`;
+    document.QuerySelector("s_a").innerHTML = `Error: {e}`;
   }
 };
